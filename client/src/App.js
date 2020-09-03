120134600288d861ac23cb0b33a6f40072186753
iming <phong.steph@gmail.com>
2020-09-03 17:30:27 +0200

    Banner as component

--- client/src/App.js
+++ client/src/App.js
@@ -2,14 +2,14 @@ import React from "react";
 import "./App.scss";
 
 import Header from "./components/Header";
+import Banner from "./components/Banner";
 import Footer from "./components/Footer";
-import Banner from "./assets/banner.jpg";
 
 function App() {
   return (
     <div className="container">
       <Header />
-      <img class="banner" src={Banner} alt="Mcdonalds banner" />
+      <Banner />
       <Footer />
     </div>
   );
---
+++ client/src/components/Banner/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+import BannerImg from "../../assets/banner.jpg";
+
+const Banner = () => {
+  return <img className="banner" src={BannerImg} alt="Mcdonalds banner" />;
+};
+
+export default Banner;
