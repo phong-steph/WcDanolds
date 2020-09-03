1bacd46f86f4abcc0ae5b88d68c839070e70dedf
iming <phong.steph@gmail.com>
2020-09-03 16:39:11 +0200

    Add server samples

--- server.js
+++ server.js
@@ -3,8 +3,10 @@ const app = express();
 
 const port = 3001;
 
+const payload = require("./samples.json");
+
 app.get("/", (req, res) => {
-  res.send({ express: "Hello From Express" });
+  res.send(payload);
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
